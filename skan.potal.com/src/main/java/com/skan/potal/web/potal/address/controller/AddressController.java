208617864e07ea0fb30a5c5ed43c9d15b4661e61
ahn <ahn@ahn-PC>
2016-01-06 00:43:05 +0900

    페이징 기능 추가

--- skan.potal.com/src/main/java/com/skan/potal/web/potal/address/controller/AddressController.java
+++ skan.potal.com/src/main/java/com/skan/potal/web/potal/address/controller/AddressController.java
@@ -18,20 +18,27 @@
 package com.skan.potal.web.potal.address.controller;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.skan.potal.web.potal.address.model.HmEmailInfo;
 import com.skan.potal.web.potal.address.model.HmMngAddress;
+import com.skan.potal.web.potal.address.repository.HmEmailInfoRepository;
 import com.skan.potal.web.potal.address.repository.HmMngAddressRepository;
+import com.skan.potal.web.potal.common.util.PageUtils;
 
 /**
  * @author ahn
@@ -43,20 +50,54 @@ public class AddressController {
 	private final Logger logger = LoggerFactory.getLogger(AddressController.class);
 	
 	@Autowired HmMngAddressRepository hmMngAddressRepository;
-	
+	@Autowired HmEmailInfoRepository hmEmailInfoRepository;
+
 	@RequestMapping("/address/address_list")
-	public String addressList(@RequestParam Integer page, 
-			@RequestParam Integer size,
-			@RequestParam Direction direction ,HttpServletResponse response , ModelMap modelMap) throws Exception {
+	public String addressList(@RequestParam(required=false , defaultValue="0") Integer page,
+			@RequestParam(required=false , defaultValue="10") Integer size,
+			@RequestParam(required=false , defaultValue="DESC") Direction direction ,HttpServletResponse response , ModelMap modelMap) throws Exception {
 		
+		Sort sort = new Sort(
+				//new org.springframework.data.domain.Sort.Order(Direction.ASC, "name"), 
+				new Order(Direction.DESC , "hmMgNum")
+				);  
 		
-		Page<HmMngAddress> hmMngAddressPage =  hmMngAddressRepository.findAll(new PageRequest(1, 10, direction , "hmMgNum"));
+		Page<HmMngAddress> hmMngAddressPage =  hmMngAddressRepository.findAll(new PageRequest(page, size, sort));
+		hmMngAddressPage.getTotalPages();
+		
+		PageUtils pageUtils = new PageUtils();
+		int current = hmMngAddressPage.getNumber() + 1;
+		//int begin = Math.max(1, current - 5);
+		int begin = pageUtils.pagingBegin(current);
+	    int end = pageUtils.pagingEnd(hmMngAddressPage.getTotalPages());
+	    
+
 		logger.debug("pageInfo = " , hmMngAddressPage);
 		
+		modelMap.put("current", current);
+		modelMap.put("begin", begin);
+		modelMap.put("end", end);
 		modelMap.put("hmMngAddressPage", hmMngAddressPage);
-		modelMap.addAttribute("returnName", "hahahaha~");
 		
-		return "";
+		return "/address/address_list.tiles";
+	}
+	
+	@RequestMapping("/address/address_insert")
+	public String addressInsert(@Valid HmMngAddress hmMngAddress, @Valid HmEmailInfo hmEmailInfo, BindingResult bindingResult,
+			HttpServletResponse response , ModelMap modelMap) throws Exception {
+		
+		if(bindingResult.hasErrors()) {
+			return "/address/address_insert.tiles";
+		}
+		
+		// 1. 주소 기본정보
+		hmMngAddressRepository.save(hmMngAddress);
+		
+		// 2. 이메일 정보 
+		hmEmailInfo.setHmMngAddress(hmMngAddress);
+		hmEmailInfoRepository.save(hmEmailInfo);
+		
+		return "redirect:/address/address_list";
 	}
 	
 }
--- skan.potal.com/src/main/webapp/WEB-INF/jsp/views/address/address_list.jsp
+++ skan.potal.com/src/main/webapp/WEB-INF/jsp/views/address/address_list.jsp
@@ -1,2 +1,105 @@
+<%@page import="com.skan.potal.web.potal.common.util.PageUtils"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="/WEB-INF/jsp/common/TagLib.jspf"%>
+
+<style>
+.table-curved {
+	border-collapse: separate;
+}
+
+.table-curved {
+	border: solid #ccc 1px;
+	border-radius: 6px;
+	border-left: 0px;
+}
+
+.table-curved td, .table-curved th {
+	border-left: 1px solid #ccc;
+	border-top: 1px solid #ccc;
+}
+
+.table-curved th {
+	border-top: none;
+}
+
+.table-curved th:first-child {
+	border-radius: 6px 0 0 0;
+}
+
+.table-curved th:last-child {
+	border-radius: 0 6px 0 0;
+}
+
+.table-curved th:only-child {
+	border-radius: 6px 6px 0 0;
+}
+
+.table-curved tr:last-child td:first-child {
+	border-radius: 0 0 0 6px;
+}
+
+.table-curved tr:last-child td:last-child {
+	border-radius: 0 0 6px 0;
+}
+</style>
+<script>
+var pagingAction = (function (){
+	return {
+		submit : function (pageNo) {
+			document.address_form.page.value = pageNo;  
+			document.address_form.submit();		
+		},
+		pre : function (beginNo) {
+			document.address_form.page.value = Number(beginNo-2);  
+			document.address_form.submit();
+		},
+		next : function (endNo) {
+			document.address_form.page.value = Number(endNo); 
+			document.address_form.submit();
+		}
+		
+	}
+})(); 
+
+</script>
+
+<form name="address_form" method="get" action="${pageContext.request.contextPath}/address/address_list">
+	<input id="page" name="page" type="hidden" value="">
+</form>
+<table class="table table-curved">
+	<caption>주소정보.</caption>
+	<thead>
+		<tr>
+			<th>번호</th>
+			<th>이름</th>
+			<th>email</th>
+			<th>Username</th>
+		</tr>
+	</thead>
+	<tbody>
+		<c:forEach var="item" items="${hmMngAddressPage.content}" varStatus="status">
+			<tr>
+				<th scope="row">${item.hmMgNum}</th>
+				<td>${item.name}</td>
+				<td>@mdo</td>
+				<td>@mdo</td>
+			</tr>
+		</c:forEach>
+	</tbody>
+</table>
+<c:choose>
+	<c:when test="${!empty hmMngAddressPage.content}">
+		<ul class="pagination">
+			<c:if test="${begin ne 1}">
+			<li><a href="#" onclick="pagingAction.pre('${begin}')">이전</a></li>
+			</c:if>
+			<c:forEach begin="${begin}" end="${end}" var="pagingNo">
+				<li class="<c:if test="${current eq pagingNo}">active</c:if>"><a href="#" onclick="pagingAction.submit('${pagingNo-1}')">${pagingNo}</a></li>
+			</c:forEach>
+			<c:if test="${hmMngAddressPage.totalPages > end }">			
+			<li><a href="#" onclick="pagingAction.next('${end}')">다음</a></li>
+			</c:if>
+			
+		</ul>
+	</c:when>
+</c:choose>
