740dd6df8ff2050cb4005336518b6354bb6cee0e
ahn <ahn@ahn-PC>
2016-01-11 15:03:17 +0900

    페이징 검색조건 추가

--- skan.potal.com/src/main/java/com/skan/potal/web/potal/address/repository/HmMngAddressRepository.java
+++ skan.potal.com/src/main/java/com/skan/potal/web/potal/address/repository/HmMngAddressRepository.java
@@ -1,5 +1,7 @@
 package com.skan.potal.web.potal.address.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.skan.potal.web.potal.address.model.HmMngAddress;
 @Repository
 public interface HmMngAddressRepository extends JpaRepository<HmMngAddress, Long> {
 
+	public Page<HmMngAddress> findByNameContaining(String name,Pageable pageable);
 }
