4cd122460842fb94c27da07e7389737027739203
skan <skan@skan-PC>
2016-01-30 19:07:59 +0900

    레파지토리 테스트코드

--- skan.potal.com/src/test/java/com/skan/potal/web/potal/code/controller/CmtbCodeRepositoryTest.java
+++ skan.potal.com/src/test/java/com/skan/potal/web/potal/code/controller/CmtbCodeRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.skan.potal.web.potal.code.controller;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +24,11 @@ public class CmtbCodeRepositoryTest {
 	@Autowired private CmtbCodeRepository cmtbCodeRepository;
 	
 	@Test
-	public void findAllTest() {
-		
-
+	public void findAllTest() throws Exception {
+		List<CmtbCode> cmtbCode =  cmtbCodeRepository.findAll();
+		cmtbCode.forEach(item -> {
+			item.getCodeName();	
+		});
 	}
 	
 	
