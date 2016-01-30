82de1c113816cfa496c1582fb22e5c0e7dbcbf77
skan <skan@skan-PC>
2016-01-30 19:11:35 +0900

    커스텀 Native Query 추가

--- skan.potal.com/src/main/java/com/skan/potal/web/potal/application/controller/CodeController.java
+++ skan.potal.com/src/main/java/com/skan/potal/web/potal/application/controller/CodeController.java
@@ -17,6 +17,8 @@
  */
 package com.skan.potal.web.potal.application.controller;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServletRequest;
@@ -31,12 +33,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.skan.potal.web.potal.application.dao.CmtbCodeRepository;
 import com.skan.potal.web.potal.application.model.CmtbCode;
@@ -75,12 +79,13 @@ public class CodeController {
 		
 		Page<CmtbCode> codePage;
 		if(StringUtils.isNotEmpty(searchName)) {
-			codePage = cmtbCodeRepository.findByUpperCode(searchName, new PageRequest(page, size, sort));
+			codePage = cmtbCodeRepository.findByCodeNameContaining(searchName, new PageRequest(page, size, sort));
 		}else {
 			codePage = cmtbCodeRepository.findAll(new PageRequest(page, size, sort));
 		}
 		
-		
+		CmtbCode cccc = cmtbCodeRepository.findByCodeInfo("00001");
+		logger.debug("dsdfsdfs = {} ",cccc);
 		PageUtils pageUtils = new PageUtils();
 		int current = codePage.getNumber() + 1;
 		//int begin = Math.max(1, current - 5);
@@ -98,6 +103,14 @@ public class CodeController {
 		return "/code/code_list.tiles";
 	}
 	
+	@RequestMapping(value="code/code_list" , produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody List<CmtbCode> codeList(){
+		
+		List<CmtbCode>  cmtbCodes = cmtbCodeRepository.findAll();
+		
+		return cmtbCodes;
+	}
+	
 	
 	/**
 	 * Code 입력 폼
@@ -106,14 +119,15 @@ public class CodeController {
 	 * @return
 	 */
 	@RequestMapping("code/code_form")
-	private String codeForm(@RequestParam(required=false) String codeMgtNo
-			, @RequestParam(required=false) String code
+	private String codeForm(
+			//@RequestParam(required=false) String codeMgtNo
+			 @RequestParam(required=false) String code
 			, ModelMap modelMap) {
 		
-		if(StringUtils.isNotEmpty(codeMgtNo)){
+		if(StringUtils.isNotEmpty(code)){
 			
 			CmtbCodeId cmtbCodeId = new CmtbCodeId();
-			cmtbCodeId.setCodeMgtNo(codeMgtNo);
+			//cmtbCodeId.setCodeMgtNo(codeMgtNo);
 			cmtbCodeId.setCode(code);
 			CmtbCode cmtbCode = cmtbCodeRepository.findOne(cmtbCodeId);
 			modelMap.put("cmtbCode", cmtbCode);
@@ -136,10 +150,11 @@ public class CodeController {
 	@RequestMapping("code/code_insert")
 	private String saveCode(@Valid CmtbCode cmtbCode ,@ModelAttribute CmtbCodeId cmtbCodeId ,BindingResult bindingResult, HttpServletRequest request, ModelMap modelMap) throws Exception {
 		
-		if(StringUtils.isEmpty(cmtbCodeId.getCodeMgtNo())|| StringUtils.isNotEmpty(cmtbCode.getUpperCode())){
+		if(StringUtils.isEmpty(cmtbCodeId.getCode())){
 			// 신규 저장
 			// 상위 코드 값이 있으면 max값 확인
-			cmtbCodeId.setCodeMgtNo(UUIDUtils.createUUID());
+//			cmtbCodeId.setCodeMgtNo(UUIDUtils.createUUID());
+			cmtbCode.setUpperCode(StringUtils.defaultIfEmpty(cmtbCode.getUpperCode(), null));
 			cmtbCode.setCodeSeq(cmtbCodeService.maxCodeSeq(cmtbCode.getUpperCode()));
 			cmtbCodeId.setCode(cmtbCodeService.lpadMaxCodeValue());
 		}
@@ -149,6 +164,7 @@ public class CodeController {
 		 
 		return "redirect:/code/code_list";
 	}
+	
 	
 	@ModelAttribute
 	public void commonAttribute(ModelMap modelMap) {
--- skan.potal.com/src/main/java/com/skan/potal/web/potal/application/dao/CmtbCodeRepository.java
+++ skan.potal.com/src/main/java/com/skan/potal/web/potal/application/dao/CmtbCodeRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import com.skan.potal.web.potal.application.model.CmtbCode;
 import com.skan.potal.web.potal.application.model.CmtbCodeId;
@@ -14,6 +16,24 @@ public interface CmtbCodeRepository extends JpaRepository<CmtbCode, CmtbCodeId>,
 	
 	public CmtbCode findByCmtbCodeId(CmtbCodeId cmtbCodeId) throws Exception;
 	public List<CmtbCode> findByUpperCode(String upperCode) throws Exception;
+	
+	/**
+	 * 코드명으로 조회
+	 * @param codeName
+	 * @param pageable
+	 * @return
+	 * @throws Exception
+	 */
+	public Page<CmtbCode> findByCodeNameContaining(String codeName   , Pageable pageable) throws Exception;
+	/**
+	 * 상위코드값을 조회
+	 * @param upperCode
+	 * @param pageable
+	 * @return
+	 * @throws Exception
+	 */
 	public Page<CmtbCode> findByUpperCode(String upperCode , Pageable pageable) throws Exception;
 	
+	@Query( value="select c.* from Cmtbcode c where c.code = :upperCode", nativeQuery=true)
+	public CmtbCode findByCodeInfo(@Param("upperCode") String upperCode);
 }
